f672de63e4fd3df991b1f1a020d16068d06b0bb8
princyemmanuel <73642019+princyemmanuel@users.noreply.github.com>
2020-11-07 22:16:39 +0530

    Delete song.js

--- routes/song.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router= new express.Router();
-const songController=require("../controllers/songController")
-
-router.route('/').post(songController.addsong)
-
-module.exports=router
